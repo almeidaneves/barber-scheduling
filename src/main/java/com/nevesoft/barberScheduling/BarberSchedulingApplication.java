210d6acf42972b1882cf8a404bb8f95e123a7abc
Adalberto Neves <nevessoka9@gmail.com>
2021-06-01 14:15:21 +0100

    Initial commit

---
+++ src/main/java/com/nevesoft/barberScheduling/BarberSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.nevesoft.barberScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberSchedulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nevesoft/barberScheduling/BarberSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevesoft.barberScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
